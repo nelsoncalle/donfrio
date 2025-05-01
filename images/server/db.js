06ba9aae837ab39e16ed313bbcd2bcb2099aef6f
Nelson <nrcalle_790@hotmail.com>
2025-05-01 16:50:12 -0500

    hola

---
+++ images/server/db.js
@@ -0,0 +1,28 @@
+const sql = require('mssql');
+
+const config = {
+    user: 'donfrio',
+    password: 'nelson',
+    server: 'localhost', // o el nombre/IP de tu servidor
+    database: 'Donfrio',
+    options: {
+        encrypt: true,
+        trustServerCertificate: true
+    }
+};
+
+
+
+
+
+// Función para conectar con la base de datos
+async function conectarDB() {
+    try {
+        await sql.connect(config);
+        console.log('✅ Conectado a SQL Server correctamente');
+    } catch (error) {
+        console.error('❌ Error en la conexión:', error);
+    }
+}
+
+module.exports = { conectarDB, sql };
