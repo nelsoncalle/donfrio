42aeaa181835cffb7014a5d36344a63c815565df
Nelson <nrcalle_790@hotmail.com>
2025-07-01 17:45:46 -0500

    d

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>DONFRIO</title>
+        <title>chiatec</title>
         <link rel="icon" href="images/DF.png" type="image/x-icon">
         <link rel="stylesheet" href="styles.css">
         <link rel="stylesheet" href="styles-compartir.css">
